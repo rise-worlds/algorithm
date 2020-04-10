2057ec5596048bc6be1f6893f725b2fb37b8d09c
rise <rise.worlds@outlook.com>
2020-04-10 11:25:22 +0800

    sync

--- dev.ps1
+++ dev.ps1
@@ -3,3 +3,5 @@ $installPath = vswhere -version 16.0 -prerelease -property installationpath
 Import-Module (Join-Path $installPath "Common7\Tools\vsdevshell\Microsoft.VisualStudio.DevShell.dll")
 Enter-VsDevShell -VsInstallPath $installPath -SkipAutomaticLocation
 code ./
+
+# "C:\Program Files\PowerShell\6\pwsh.exe" -noe -c "&{Import-Module """C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\Tools\Microsoft.VisualStudio.DevShell.dll"""; Enter-VsDevShell a4017e9f -SkipAutomaticLocation}"
